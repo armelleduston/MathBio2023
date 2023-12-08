83160838c0a03b3b8ef7eea73e3066d5cceb789d
lbandy0311 <121705652+lbandy0311@users.noreply.github.com>
2023-12-08 16:21:16 -0700

    Update find_EP.m
    added function to look at differences between each set of 2 fireflies

--- find_EP.m
+++ find_EP.m
@@ -5,11 +5,17 @@ A = 0.7;
 
 x0 = [w1; w2; w3];
 
+% llama model
 F = @(x) [w1 + (A/3)*(sin(x(2)-x(1)) + sin(x(3)-x(1)));
           w2 + (A/3)*(sin(x(1)-x(2)) + sin(x(3)-x(2)));
           w3 + (A/3)*(sin(x(1)-x(3)) + sin(x(2)-x(3)))];
 
-EP = fsolve(F, x0)
+% pairwise differences
+f = @(x) [(w1 + (A/3)*(sin(x(2)-x(1)) + sin(x(3)-x(1))))-(w2 + (A/3)*(sin(x(1)-x(2)) + sin(x(3)-x(2))));
+          (w2 + (A/3)*(sin(x(1)-x(2)) + sin(x(3)-x(2))))-(w3 + (A/3)*(sin(x(1)-x(3)) + sin(x(2)-x(3))));
+          (w3 + (A/3)*(sin(x(1)-x(3)) + sin(x(2)-x(3))))-(w1 + (A/3)*(sin(x(2)-x(1)) + sin(x(3)-x(1))))];
+
+EP = fsolve(f, x0)
 
 
 J = [-(A/3)*(cos(EP(2)-EP(1)) + cos(EP(3)-EP(1))), (A/3)*cos(EP(2)-EP(1)), (A/3)*cos(EP(3)-EP(1));
